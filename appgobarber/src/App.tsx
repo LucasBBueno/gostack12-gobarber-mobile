9a2b0cad835d951df0183846bcdf310f3fda975e
Lucas Bueno <lucas-jbb@hotmail.com>
2020-08-18 11:06:46 -0300

    Add forgotpassword and createaccount button

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -8,7 +8,7 @@ import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View
       style={{
         flex: 1,
