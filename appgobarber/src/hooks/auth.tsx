09eb6c9b87906e698de3a7b78a9aebdff6fb8af8
Lucas Bueno <lucas-jbb@hotmail.com>
2020-08-19 10:05:50 -0300

    Create appRoutes for authenticated user

--- appgobarber/src/hooks/auth.tsx
+++ appgobarber/src/hooks/auth.tsx
@@ -21,6 +21,7 @@ interface SignCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignCredentials): Promise<void>;
   signOut(): void;
 }
@@ -29,6 +30,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -43,6 +45,8 @@ const AuthProvider: React.FC = ({ children }) => {
           user: JSON.parse(user[1]),
         });
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -74,7 +78,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
